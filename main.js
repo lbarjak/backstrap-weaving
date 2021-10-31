91d50d2553503809be7c13cbdd6a89b8a5354ea2
lbarjak <barjak.laszlo@gmail.com>
2021-10-31 12:22:48 +0100

    refactored

--- main.js
+++ main.js
@@ -21,58 +21,59 @@ export default class Main {
     this.drawing = SVG().addTo("#backstrap").size(600, 300);
     this.rect = this.drawing.rect(600, 300).attr({ fill: "gray" });
     this.polygons = [];
-    this.main();
+    this.init();
   }
-  main() {
-    var row = 0,
-      pos = 0,
-      timer;
-    var pattNow = Main.patterns[Main.nameOfPattern];
-    var corr = pattNow.upper.length === pattNow.lower.length ? 4 : 0;
 
-    const draw = () => {
-      var dir = row % 2 ? -1 : 1;
-      var x,
-        y = 4 + row * 41,
-        color;
-      var pattern = pattNow[Main.patterns.healds[row % 2]];
-      x = 299 - pattern.length * 8;
-      x = x + pos * 16 + dir * corr * -1;
-      color = Main.patterns.colors[pattern[pos]];
-      let polygon = new Polygon(this.drawing, x, y, color);
-      this.polygons.push(polygon);
-      pos = pos + dir;
-      if (pos === pattern.length || pos === -1)
-        pos =
-          (++row % 2) *
-          (Main.patterns[Main.nameOfPattern][Main.patterns.healds[row % 2]]
-            .length -
-            1);
-      timer = setTimeout(() => draw(), 30);
-      if (row === 7) {
-        clearTimeout(timer);
-        row = 0;
-        pos = 0;
-        printPolygons();
-      }
-    };
-
-    draw();
-
-    let printPolygons = () => {
-      this.polygons.forEach((polygon) =>
-        console.log(polygon.serNum, polygon.x, polygon.y)
-      );
-    };
+  init() {
+    this.row = 0;
+    this.pos = 0;
+    this.timer;
+    this.pattNow = Main.patterns[Main.nameOfPattern];
+    this.corr = this.pattNow.upper.length === this.pattNow.lower.length ? 4 : 0;
 
     this.rect.click(() => {
-      if (!row && !pos) {
+      if (!this.row && !this.pos) {
         this.polygons = [];
         Polygon.serNum = 0;
         let hexagons = document.querySelectorAll("polygon");
         hexagons.forEach((polygon) => polygon.remove());
-        draw();
+        this.draw();
       }
     });
+
+    this.draw();
   }
+
+  draw = () => {
+    let dir = this.row % 2 ? -1 : 1;
+    let y = 4 + this.row * 41;
+    let pattern = this.pattNow[Main.patterns.healds[this.row % 2]];
+    let x = 299 - pattern.length * 8;
+    x = x + this.pos * 16 + dir * this.corr * -1;
+    let color = Main.patterns.colors[pattern[this.pos]];
+    let polygon = new Polygon(this.drawing, x, y, color);
+    this.polygons.push(polygon);
+    this.pos = this.pos + dir;
+    if (this.pos === pattern.length || this.pos === -1)
+      this.pos =
+        (++this.row % 2) *
+        (Main.patterns[Main.nameOfPattern][Main.patterns.healds[this.row % 2]]
+          .length -
+          1);
+
+    this.timer = setTimeout(() => this.draw(), 30);
+
+    if (this.row === 7) {
+      clearTimeout(this.timer);
+      this.row = 0;
+      this.pos = 0;
+      //this.printPolygons();
+    }
+  };
+
+  printPolygons = () => {
+    this.polygons.forEach((polygon) =>
+      console.log(polygon.serNum, polygon.x, polygon.y)
+    );
+  };
 }
