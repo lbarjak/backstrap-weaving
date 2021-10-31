faa1bbf0e7005c83c301b4be9cfad890daa47c71
lbarjak <barjak.laszlo@gmail.com>
2021-10-31 11:24:25 +0100

    refactored

--- polygon.js
+++ polygon.js
@@ -1,16 +1,6 @@
 export default class Polygon {
   static serNum = 0;
-
-  constructor(drawing, x, y, color) {
-    this.serNum = Polygon.serNum++;
-    this.drawing = drawing;
-    this.x = x;
-    this.y = y;
-    this.color = color;
-    this.poly;
-  }
-
-  hex = (x, y) => [
+  static hex = (x, y) => [
     [x + 0, y + 7],
     [x + 7, y + 0],
     [x + 14, y + 7],
@@ -19,17 +9,22 @@ export default class Polygon {
     [x + 0, y + 39],
   ];
 
+  constructor(drawing, x, y, color) {
+    this.serNum = Polygon.serNum++;
+    this.drawing = drawing;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
   hexagon = () => {
-    this.poly = this.drawing.polygon(this.hex(this.x, this.y));
-    this.poly.on("click", (e) =>
+    let poly = this.drawing.polygon(Polygon.hex(this.x, this.y));
+    poly.on("click", (e) =>
       console.log(
-        "clicked",
-        this.serNum,
-        e.target.points[0].x,
-        e.target.points[0].y
+        `Clicked! serNum: ${this.serNum}; x=${e.target.points[0].x}; y=${e.target.points[0].y}`
       )
     );
-    this.poly.fill(this.color);
-    this.poly.stroke({ width: 0 });
+    poly.fill(this.color);
+    poly.stroke({ width: 0 });
   };
 }
