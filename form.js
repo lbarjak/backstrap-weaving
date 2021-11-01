0c47ca32192fea30c6996818a0c2bff1d2687fcc
lbarjak <barjak.laszlo@gmail.com>
2021-11-02 00:14:45 +0100

    form added

---
+++ form.js
@@ -0,0 +1,35 @@
+import Main from "./main.js";
+export default class Form {
+  constructor(self) {
+    this.self = self;
+    this.form();
+  }
+
+  form() {
+    let nameOfPattern = "hullámos";
+    let patterns = Main.backstraps.patterns;
+    let menu = document.getElementById("menu");
+    menu.append("Válassz egy mintát:");
+    let form = document.createElement("form");
+    menu.append(form);
+    let select = document.createElement("select");
+    form.append(select);
+    let option;
+    for (const [key] of Object.entries(patterns)) {
+      option = document.createElement("option");
+      option.value = key;
+      option.append(key);
+      select.append(option);
+    }
+    let button = document.createElement("input");
+    button.type = "button";
+    button.value = "Rajzold!";
+    form.append(button);
+    form.addEventListener("change", (e) => {
+      nameOfPattern = e.target.value;
+    });
+    button.addEventListener("click", (e) => {
+      this.self.init(nameOfPattern);
+    });
+  }
+}
--- main.js
+++ main.js
@@ -1,65 +1,70 @@
 // Barják László, 2017.06.10.
 import Polygon from "./polygon.js";
+import Form from "./form.js";
 
 export default class Main {
-  static patterns = {
-    hullamos: { upper: "vvvvvsvvvv", lower: "vvvvsvvvvv" },
-    lancos: { upper: "vvvvsvsvvvv", lower: "vvvvssvvvv" },
-    csikos: { upper: "vvssssssssssvv", lower: "vvsssssssssvv" },
-    keresztcsikos: { upper: "vvvvvvvvvvvvvvvvv", lower: "ssssssssssssssss" },
-    kigyohatas: {
-      upper: "ssssvvvvvvvsssvsssvvvvvvvssss",
-      lower: "ssssvvvssssssvvssssssvvvssss",
+  static backstraps = {
+    patterns: {
+      hullámos: { upper: "vvvvvsvvvv", lower: "vvvvsvvvvv" },
+      láncos: { upper: "vvvvsvsvvvv", lower: "vvvvssvvvv" },
+      csíkos: { upper: "vvssssssssssvv", lower: "vvsssssssssvv" },
+      keresztcsíkos: { upper: "vvvvvvvvvvvvvvvvv", lower: "ssssssssssssssss" },
+      kígyóhátas: {
+        upper: "ssssvvvvvvvsssvsssvvvvvvvssss",
+        lower: "ssssvvvssssssvvssssssvvvssss",
+      },
+      keresztes: { upper: "vvvsssvvsssvvsssvvv", lower: "sssvvsssvvsssvvsss" },
     },
-    keresztes: { upper: "vvvsssvvsssvvsssvvv", lower: "sssvvsssvvsssvvsss" },
     colors: { v: "white", s: "red" },
     healds: { 0: "upper", 1: "lower" },
   };
-  static nameOfPattern = "kigyohatas";
+  static nameOfPattern = "kígyóhátas";
 
   constructor() {
     this.drawing = SVG().addTo("#backstrap").size(600, 600);
     this.rect = this.drawing.rect(600, 600).attr({ fill: "gray" });
     this.polygons = [];
-    this.init();
-  }
-
-  init() {
+    new Form(this);
     this.row = 0;
     this.pos = 0;
     this.timer;
-    this.pattNow = Main.patterns[Main.nameOfPattern];
-    this.corr = this.pattNow.upper.length === this.pattNow.lower.length ? 4 : 0;
+    this.init("hullámos");
+  }
 
-    this.rect.click(() => {
-      if (!this.row && !this.pos) {
-        this.polygons = [];
-        Polygon.serNum = 0;
-        let hexagons = document.querySelectorAll("polygon");
-        hexagons.forEach((polygon) => polygon.remove());
-        this.draw();
-      }
-    });
+  reset = () => {
+    clearTimeout(this.timer);
+    this.row = 0;
+    this.pos = 0;
+    this.polygons = [];
+    Polygon.serNum = 0;
+    let hexagons = document.querySelectorAll("polygon");
+    hexagons.forEach((polygon) => polygon.remove());
+    hexagons = document.querySelectorAll("polygon");
+  };
 
+  init(nameOfPattern) {
+    this.row = 0;
+    this.pos = 0;
+    this.reset();
+    this.pattNow = Main.backstraps.patterns[nameOfPattern];
     this.draw();
   }
 
   draw = () => {
+    this.corr = this.pattNow.upper.length === this.pattNow.lower.length ? 4 : 0;
     let dir = this.row % 2 ? -1 : 1;
     let y = 10 + this.row * 41;
-    let pattern = this.pattNow[Main.patterns.healds[this.row % 2]];
+    let pattern = this.pattNow[Main.backstraps.healds[this.row % 2]];
     let x = 299 - pattern.length * 8;
     x = x + this.pos * 16 + dir * this.corr * -1;
-    let color = Main.patterns.colors[pattern[this.pos]];
+    let color = Main.backstraps.colors[pattern[this.pos]];
     let polygon = new Polygon(this.drawing, x, y, color);
     this.polygons.push(polygon);
     this.pos = this.pos + dir;
     if (this.pos === pattern.length || this.pos === -1)
       this.pos =
         (++this.row % 2) *
-        (Main.patterns[Main.nameOfPattern][Main.patterns.healds[this.row % 2]]
-          .length -
-          1);
+        (this.pattNow[Main.backstraps.healds[this.row % 2]].length - 1);
 
     this.timer = setTimeout(() => this.draw(), 20);
 
@@ -67,13 +72,6 @@ export default class Main {
       clearTimeout(this.timer);
       this.row = 0;
       this.pos = 0;
-      //this.printPolygons();
     }
-  };
-
-  printPolygons = () => {
-    this.polygons.forEach((polygon) =>
-      console.log(polygon.serNum, polygon.x, polygon.y)
-    );
   };
 }
