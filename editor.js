2b8b0a5cadd3fd405a12eab45b5dbe086bc38377
lbarjak <barjak.laszlo@gmail.com>
2021-11-03 01:23:02 +0100

    editor first row

--- editor.js
+++ editor.js
@@ -14,12 +14,41 @@ export default class Editor {
     this.rect.on("mousedown", (e) => this.hexagon());
   }
 
-  hexagon() {
-    let poly;
+  hexagon = () => {
+    let color = "white";
     for (let i = 0; i < 37; i++) {
-      poly = this.editor.polygon(Editor.hex(5 + 16 * i, 10));
-      poly.attr({ "fill-opacity": 0 });
-      poly.stroke({ color: "white", width: 1 });
+      let poly = this.editor.polygon(Editor.hex(5 + 16 * i, 10));
+      poly.on("click", (e) => {
+        console.log("clicked", poly.attr("fill"));
+        if (poly.attr("fill") === "#000000") {
+          poly.attr({
+            "fill-opacity": 1,
+            "stroke-width": 0,
+            fill: color,
+          });
+        } else {
+          poly.attr({
+            "fill-opacity": 1,
+            "stroke-width": 0,
+          });
+          poly.fill((color = poly.attr("fill") === "white" ? "red" : "white"));
+        }
+      });
+      poly.on("contextmenu", (e) => {
+        e.preventDefault();
+        poly.attr({
+          "fill-opacity": 0.0001,
+          stroke: "white",
+          "stroke-width": 1,
+        });
+      });
+      poly.attr({
+        "fill-opacity": 0.0001,
+        stroke: "white",
+        "stroke-width": 1,
+      });
+      //poly.fill("white");
+      //poly.stroke({ color: "white", width: 1 });
     }
-  }
+  };
 }
